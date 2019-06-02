afcbc6714c7f1bbaf5fb923ec39d37fcfa2005d0
zhanglin2018 <893330264@qq.com>
2019-06-02 17:21:58 +0800

    PartV: realize the function of zoomin and zoomout, add the keyhandler.

--- com.advantest.gef/src/com/advantest/gef/editor/MyGraphicalEditor.java
+++ com.advantest.gef/src/com/advantest/gef/editor/MyGraphicalEditor.java
@@ -1,10 +1,23 @@
 package com.advantest.gef.editor;
 
+import java.util.ArrayList;
+
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.draw2d.geometry.Rectangle;
 import org.eclipse.gef.DefaultEditDomain;
 import org.eclipse.gef.GraphicalViewer;
+import org.eclipse.gef.KeyHandler;
+import org.eclipse.gef.KeyStroke;
+import org.eclipse.gef.MouseWheelHandler;
+import org.eclipse.gef.MouseWheelZoomHandler;
+import org.eclipse.gef.editparts.ScalableRootEditPart;
+import org.eclipse.gef.editparts.ZoomManager;
+import org.eclipse.gef.ui.actions.GEFActionConstants;
+import org.eclipse.gef.ui.actions.ZoomInAction;
+import org.eclipse.gef.ui.actions.ZoomOutAction;
 import org.eclipse.gef.ui.parts.GraphicalEditor;
+import org.eclipse.swt.SWT;
+import org.eclipse.ui.actions.ActionFactory;
 
 import com.advantest.gef.editpart.AppEditPartFactory;
 import com.advantest.gef.model.Employe;
@@ -86,5 +99,54 @@ public class MyGraphicalEditor extends GraphicalEditor {
 		super.configureGraphicalViewer();
 		GraphicalViewer viewer = getGraphicalViewer();
 		viewer.setEditPartFactory(new AppEditPartFactory());
+		
+		/*
+		 * zoomin and zoomout
+		 */
+		
+		double[] zoomLevels = new double[] {0.25, 0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0, 3.0, 4.0, 5.0};
+		ArrayList<String> zoomContributions = new ArrayList<String>();
+		ScalableRootEditPart rootEditPart = new ScalableRootEditPart();
+		viewer.setRootEditPart(rootEditPart);
+		
+		ZoomManager manager = rootEditPart.getZoomManager();
+		getActionRegistry().registerAction(new ZoomInAction(manager));
+		getActionRegistry().registerAction(new ZoomOutAction(manager));
+		
+		manager.setZoomLevels(zoomLevels);
+		
+		zoomContributions.add(ZoomManager.FIT_ALL);
+		zoomContributions.add(ZoomManager.FIT_HEIGHT);
+		zoomContributions.add(ZoomManager.FIT_WIDTH);
+		manager.setZoomLevelContributions(zoomContributions);
+		
+		/*
+		 * key handler
+		 */
+		KeyHandler keyHandler = new KeyHandler();
+		keyHandler.put(KeyStroke.getPressed(SWT.DEL, 127, 0),
+				getActionRegistry().getAction(ActionFactory.DELETE.getId()));
+
+		keyHandler.put(KeyStroke.getPressed('+', SWT.KEYPAD_ADD, 0),
+				getActionRegistry().getAction(GEFActionConstants.ZOOM_IN));
+		
+		keyHandler.put(KeyStroke.getPressed('-', SWT.KEYPAD_SUBTRACT, 0),
+				getActionRegistry().getAction(GEFActionConstants.ZOOM_OUT));
+		
+		viewer.setProperty(MouseWheelHandler.KeyGenerator.getKey(SWT.NONE),
+				MouseWheelZoomHandler.SINGLETON);
+		
+		viewer.setKeyHandler(keyHandler);
+		
+		
+	}
+	
+	@Override
+	public Object getAdapter(Class type) {
+		if (type == ZoomManager.class) {
+			return ((ScalableRootEditPart)getGraphicalViewer().getRootEditPart()).getZoomManager();
+		}else {
+			return super.getAdapter(type);
+		}
 	}
 }
